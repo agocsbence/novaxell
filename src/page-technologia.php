0cb9e6fd848d04479c11083c21b7952d4ddfb955
Bence Agocs <bence@harom.studio>
2021-01-20 16:21:32 +0100

    tech details component created

--- src/page-technologia.php
+++ src/page-technologia.php
@@ -129,51 +129,7 @@ get_header();
             <p class="subtitle">A számok is mellettünk állnak. Ismerd meg közelebbről a NovaXell egyedülálló tulajdonságait!</p>
         </div>
         <div class="grid grid-2-3">
-            <div class="tech-details bordered">
-                <div class="grid grid-3-2">
-                    <div class="detail-name">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/fire.svg" alt="">
-                        Tűzveszélyességi osztály:
-                    </div>
-                    <div class="detail-data" style="color: #FF002B">B2</div>
-                    
-                    <div class="detail-name">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/temperature-up.svg" alt="">
-                        Hővezetési tényező:
-                    </div>
-                    <div class="detail-data" style="color: #FF8731">λ10tr = 0,039 W/mK</div>
-                    
-                    <div class="detail-name">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/atom-alt.svg" alt="">
-                        Diffúziós együttható:
-                    </div>
-                    <div class="detail-data" style="color: #5200ff">ľ 1-2</div>
-
-                    <div class="detail-name">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/wind.svg" alt="">
-                        Befúvási sűrűség:
-                    </div>
-                    <div class="detail-data" style=""></div>
-
-                    <div class="detail-name padding-left">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/mountain.svg" alt="">
-                        Szabadon felfekvő:
-                    </div>
-                    <div class="detail-data" style="color: #1EA554">25-35 kg/m3</div>
-
-                    <div class="detail-name padding-left">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/chimney.svg" alt="">
-                        Befúvás a tetőtérbe:
-                    </div>
-                    <div class="detail-data" style="color: #866161">40-55 kg/m3</div>
-
-                    <div class="detail-name padding-left">
-                        <img src="<?php bloginfo('template_url') ?>/assets/img/icon/home.svg" alt="">
-                        Befúvás a falakba:
-                    </div>
-                    <div class="detail-data" style="color: #76A2C5">45-65 kg/m3</div>
-                </div>
-            </div>
+            <?php include get_theme_file_path( '/includes/tech-details.php' ); ?>
             <div class="downloads">
                 <a href="" class="btn btn-download" download>
                     <div class="btn-text img-right">
