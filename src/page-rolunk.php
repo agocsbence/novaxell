0ce79ffe998d257e431ae3a85e22655176b9af1a
Bence Agocs <bence@harom.studio>
2021-01-22 15:24:06 +0100

    rolunk page updated

--- src/page-rolunk.php
+++ src/page-rolunk.php
@@ -19,10 +19,6 @@ get_header();
                 <h1><?php echo $hero["blokk_cim"] ?></h1>
                 <p><?php echo $hero["blokk_leiras"] ?></p>
             <?php endif; ?>
-            <h2 class="lead"><span>\</span> A név kötelez</h2>
-            <h1>Hagyomány és innováció</h1>
-            <p>Minden a családunk 30 éves építőipari tapasztalatával kezdődöt, és egy közös TDK dolgozattal.</p>
-            <p>Édesanyánk lakberendezői és Édesapánk építőmérnöki tudását is kamatoztatva alkottuk meg azt a dolgozatot, amelyből kinőtt a NovaXell hőszigetelés. Dr. Becker Gábor, a Budapesti Műszaki és Gazdaságtudományi Egyetem professzorának tanácsai és Imre bátyám tájépítész, építésvezetői kompetenciája segítségével sikerült a piac minden igényét figyelembe vennünk, amikor véglegesítettük a jövő hőszigetelő megoldását.</p>
         </div>
         <div class="last-block image-block">
             <img src="<?php bloginfo('template_url') ?>/assets/img/blocks/about.png" alt="">
@@ -32,41 +28,42 @@ get_header();
 
 <section class="container section-about-team bg--pureWhite">
     <div class="text-center">
-        <h2 class="lead"><span>\</span> A NovaXell Alapítói</h2>
-        <h1>Cégünket egy hiánypótló megoldás kifejlesztése érdekében alapítottuk meg.</h1>
-        <p class="subtitle">Az ökologikus szemlélet kiemelkedően fontos számunkra: a fenntartható jövő hőszigetelését szeretnénk széles körben elérhetővé tenni. Ezért létrehoztuk a terméket, amely költséghatékonyan és környezetkímélően oldja meg a könnyűszerkezetes épületek nyári hővédelmi problémáit, és kiaknázza a cellulóz hőszigetelésben rejlő lehetőségeket.</p>
+        <?php
+        $team = get_field('csapat_blokk');
+        if( $team ): ?>
+            <h2 class="lead"><span>\</span> <?php echo $team["blokk_alcim"] ?></h2>
+            <h1><?php echo $team["blokk_cim"] ?></h1>
+            <p><?php echo $team["blokk_leiras"] ?></p>
+        <?php endif; ?>
     </div>
     <div class="grid grid-2">
-        <div class="image-card bordered">
-            <div class="card-image">
-                <img class="member" src="<?php bloginfo('template_url') ?>/assets/img/team-1.png" alt="">
-                <img class="logo" src="<?php bloginfo('template_url') ?>/assets/img/logo-mini-white.png" alt="">
-            </div>
-            <div class="card-content">
-                <h2 class="title">
-                    Andrási Alex
-                    <span>CEO</span>
-                </h2>
-                <p class="description">
-                    Okleveles építészmérnök, főként épületszerkezeti tervezéssel foglalkozik. Elkötelezett az épületek energiahatékony és ökologikus szemléletű tervezése mellett. Az innovatív és fenntartható építőipari technológiákat részesíti előnyben, a kreatív műszaki megoldások kiaknázását és fejlesztését tekinti fő feladatának.
-                </p>
-            </div>
-        </div>
-        <div class="image-card bordered">
-            <div class="card-image">
-                <img class="member" src="<?php bloginfo('template_url') ?>/assets/img/team-2.png" alt="">
-                <img class="logo" src="<?php bloginfo('template_url') ?>/assets/img/logo-mini.png" alt="">
-            </div>
-            <div class="card-content">
-                <h2 class="title">
-                    Andrási Imre Attila
-                    <span>CTO</span>
-                </h2>
-                <p class="description">
-                    Tájrendező-kertépítő mérnök, így az épített és természetes környezetünk harmóniájának megteremtése a szakmai hivatása. Építésvezetői tapasztalatai és kiváló szervezőképessége révén sikeres projektlebonyolító. Zöldterület- és épületfenntartásban, tervezésben és kivitelezésben is jártas. A környezettudatos, gazdaságos és kifogástalan minőségű építés számára az egyetlen járható út.
-                </p>
-            </div>
-        </div>
+
+        <?php if( have_rows('csapat') ): ?>
+			<?php while( have_rows('csapat') ): the_row();  ?>
+                <div class="image-card bordered">
+                    <div class="card-image">
+                        <img class="member" src="<?php the_sub_field('kep') ?>" alt="<?php the_sub_field('nev') ?>">
+                        
+                        <?php if (get_field('logo_szine') == 'feher') { ?>
+                            <img class="logo" src="<?php bloginfo('template_url') ?>/assets/img/logo-mini-white.png" alt="">
+                        <?php } else { ?>
+                            <img class="logo" src="<?php bloginfo('template_url') ?>/assets/img/logo-mini.png" alt="">
+                        <?php } ?>
+
+                    </div>
+                    <div class="card-content">
+                        <h2 class="title">
+                            <?php the_sub_field('nev') ?>
+                            <span><?php the_sub_field('titulus') ?></span>
+                        </h2>
+                        <p class="description">
+                            <?php the_sub_field('bemutatkozas') ?>
+                        </p>
+                    </div>
+                </div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+
     </div>
 </section>
 
