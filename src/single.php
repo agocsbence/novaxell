03d775f0c0a4bc77efda6a660061e8ff820db5b9
Bence Agocs <bence@harom.studio>
2021-01-22 17:03:11 +0100

    single page updated

--- src/single.php
+++ src/single.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+<div class="container single-page">
+    <div class="single-page-header">
+        <img src="<?php echo the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="single-page-header-image">
+        <?php the_title(); ?>
+    </div>
+    <div class="single-page-content">
+        <?php the_content(); ?>
+    </div>
+</div>
 
 <?php get_footer();
