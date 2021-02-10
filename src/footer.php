399d0d3492c46ad50d3ae11cd74ee5cca92c2f37
Bence Agocs <bence@harom.studio>
2021-02-10 11:50:21 +0100

    hiventures components added

--- src/footer.php
+++ src/footer.php
@@ -1,12 +1,17 @@
 </main>
 		<footer class="bg--green">
 
+			<img src="<?php bloginfo('template_url') ?>/assets/img/eublokk.png" alt="Széchenyi 2020" class="eublock">
+
 			<div class="container">
 				<div class="footer-content grid grid-5">
 					<div class="footer-logo">
 						<img src="<?php bloginfo('template_url') ?>/assets/img/footer-logo.svg" alt="">
 						<p class="lead">Innováció a hőszigetelésben</p>
 						<p>Új generációs, környezetkímélő hőszigetelés télen-nyáron</p>
+						<div class="hiventures">
+							<img src="<?php bloginfo('template_url') ?>/assets/img/hiventures.png" alt="Hiventures">
+						</div>
 					</div>
 					<ul id="footer-menu-nav"><div class="lead">Navigáció</div>
 						<li><a href="<?php echo esc_url( get_page_link( 23 ) ); ?>">Főoldal</a></li>
